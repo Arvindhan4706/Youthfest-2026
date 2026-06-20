c0b40be486da52aca8ca05338c1efce1f034f317
Arvindhan4706 <arvindhanmaran@gmail.com>
2026-06-21 01:10:19 +0530

    feat: Fix typescript errors and adjust intro timing for Vercel deploy

--- src/components/ui/ParticleUniverse.tsx
+++ src/components/ui/ParticleUniverse.tsx
@@ -3,6 +3,7 @@
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial } from '@react-three/drei';
+// @ts-ignore
 import * as random from 'maath/random/dist/maath-random.esm';
 import * as THREE from 'three';
 
